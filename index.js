c90dc7921f2679795e4df553d5f056850a86d10e
dlockli <92403724+dlockli@users.noreply.github.com>
2021-11-17 11:52:01 -0600

    installed apm

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// Add this to the VERY top of the first file loaded in your app
+var apm = require('elastic-apm-node').start({
+
+  // Override the service name from package.json
+  // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+  serviceName: '',
+  
+  // Use if APM Server requires a secret token
+  secretToken: 'tiy1GA4iUYgOB1UbUh',
+  
+  // Set the custom APM Server URL (default: http://localhost:8200)
+  serverUrl: 'https://a77ba09d3ccd4ff98ed3adf816fa5c20.apm.us-central1.gcp.cloud.es.io:443',
+  
+  // Set the service environment
+  environment: 'production'
+  })
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
